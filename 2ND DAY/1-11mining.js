037cf28c09d990738af3e0dd4bf89faa42317d1f
OnurKISACIK <onurkisacik1985@gmail.com>
2025-01-15 18:33:47 +0200

    New

--- 2ND DAY/1-11mining.js
+++ 2ND DAY/1-11mining.js
@@ -1,13 +1,13 @@
 function miningOne(input) {
   let x = input.length;
-
   let bitcountCounter = 0;
   let currentDay = input.length - 2;
   let totalAmount = 0;
   let firstBitcoinPurchased = "";
+
   for (let i = 0; i < x; i++) {
     let dailyGold = input[i];
-    if (currentDay % 3 === 0) {
+    if (currentDay === 3) {
       dailyGold = dailyGold * 0.7;
     }
     let totalEarnedMoneyFromGOld = dailyGold * 67.51;
@@ -17,19 +17,16 @@ function miningOne(input) {
       if (bitcountCounter === 0) {
         firstBitcoinPurchased = currentDay;
       }
-
+      bitcountCounter++;
       totalAmount -= 11949.16;
     }
     currentDay++;
-    bitcountCounter++;
-    if (bitcountCounter > 0) {
-      console.log(
-        `Day of the first purchased bitcoin: ${firstBitcoinPurchased}`
-      );
-    }
+  }
+  console.log(`Bought bitcoins: ${bitcountCounter}`);
+  if (bitcountCounter > 0) {
+    console.log(`Day of the first purchased bitcoin: ${firstBitcoinPurchased}`);
   }
 
-  console.log(`Bought bitcoins: ${bitcountCounter}`);
   console.log(`Left money: ${totalAmount.toFixed(2)} lv.`);
 }
 miningOne([100, 200, 300]);
