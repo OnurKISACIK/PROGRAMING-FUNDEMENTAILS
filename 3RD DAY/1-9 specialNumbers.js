02bf7247103c2d55d0810f15dd35affbef4578b3
OnurKISACIK <onurkisacik1985@gmail.com>
2025-01-17 22:00:38 +0200

    NEW UPDATE

--- 3RD DAY/1-9 specialNumbers.js
+++ 3RD DAY/1-9 specialNumbers.js
@@ -2,16 +2,18 @@ function specialNumbers(number) {
   for (let i = 1; i <= number; i++) {
     let c = 0;
     c += i;
-    let b = sum;
-    let sum =
+    let b = c;
+    // sum in toplamindaki sayilarin 5,7,11 e bolune bilmesi gerekli
+    if (
       b.toString().includes("5") ||
       b.toString().includes("7") ||
-      b.toString().includes("11");
-    if (result) {
-      console.log(`${i} -> True`);
-    } else {
-      console.log(`${i} -> False`);
-    }
+      b.toString().includes("11")
+  
   }
 }
 specialNumbers(15);
+  /*) {
+      console.log(`${i} -> True`);
+    } else {
+      console.log(`${i} -> False`);
+    }*/
