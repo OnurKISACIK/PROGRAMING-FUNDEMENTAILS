3528e90f466f49295047d29fe3129fd9c1848aae
OnurKISACIK <onurkisacik1985@gmail.com>
2025-01-18 13:43:22 +0200

    NEW

--- 4TH DAY/1-10 spiceMustFlow.js
+++ 4TH DAY/1-10 spiceMustFlow.js
@@ -1,2 +1,18 @@
-function spiceMustFlow() {}
-spiceMustFlow(111);
+function spiceMustFlow(yeild) {
+  let spice = Math.abs(yeild);
+  let consumed = 26;
+  let total = 0;
+  let dayCount = 0;
+  let leftamount = 0;
+
+  for (let yeild = spice; yeild >= 100; yeild -= 10) {
+    total = yeild - consumed;
+    leftamount += total;
+
+    dayCount++;
+  }
+  leftamount -= consumed;
+  console.log(dayCount);
+  console.log(leftamount);
+}
+spiceMustFlow(-450);
