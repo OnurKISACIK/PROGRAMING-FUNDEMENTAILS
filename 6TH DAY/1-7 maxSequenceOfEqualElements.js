b398625000656cddc3b425c49eec0559791f02cf
OnurKISACIK <onurkisacik1985@gmail.com>
2025-01-29 22:42:17 +0200

    NEW

--- 6TH DAY/1-7 maxSequenceOfEqualElements.js
+++ 6TH DAY/1-7 maxSequenceOfEqualElements.js
@@ -1,2 +1,4 @@
-function maxSequenceOfEqualElements() {}
+function maxSequenceOfEqualElements() {
+  f;
+}
 maxSequenceOfEqualElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
--- 6TH DAY/1-8 MagicSum.js
+++ 6TH DAY/1-8 MagicSum.js
@@ -1,2 +1,13 @@
-function magicSum() {}
-magicSum();
+/*function magicSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      let num1 = arr[i];
+      let num2 = arr[j];
+      if (num1 + num2 === target) {
+        console.log(`${num1} ${num2}`);
+      }
+    }
+  }
+}
+magicSum([1, 7, 6, 2, 19, 23], 8);
+*/
