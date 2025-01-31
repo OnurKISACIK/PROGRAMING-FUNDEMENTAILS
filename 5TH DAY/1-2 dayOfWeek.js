2c60954915e8b9a7820ee92ec11509ac94d60ed0
OnurKISACIK <onurkisacik1985@gmail.com>
2025-01-31 16:39:39 +0200

    NEW

--- 5TH DAY/1-2 dayOfWeek.js
+++ 5TH DAY/1-2 dayOfWeek.js
@@ -1,3 +1,4 @@
+/*
 function dayOfWeek(day) {
   let days = [
     "Monday",
@@ -18,3 +19,25 @@ function dayOfWeek(day) {
 dayOfWeek(3);
 dayOfWeek(6);
 dayOfWeek(8);
+*/
+function dayOfWeek(day) {
+  let weekDays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  /*
+  if (day < 0 || day > 7) {
+    console.log("Invalid Choose");
+  } else {
+    console.log(weekDays[day - 1]);
+  }
+  */
+  let result = day < 0 || day > 7 ? "Invalid Choose" : weekDays[day - 1];
+  console.log(result);
+}
+dayOfWeek(4);
---
+++ 5TH DAY/1.9 testFolder.js
@@ -0,0 +1,15 @@
+/*
+let arr = [1, 2, 3, 4, 5];
+arr[5] = 6;
+console.log(arr);
+let arr1 = [1, 2, 3];
+arr1[50] = 10;
+console.log(arr1);
+*/
+function a() {
+  let arr = [1, 2, 3];
+  arr.push(4);
+  arr.push(7);
+  console.log(arr);
+}
+a();
