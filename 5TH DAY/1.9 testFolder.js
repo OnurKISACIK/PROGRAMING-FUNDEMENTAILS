c79bc1ed3b4efc638c51c279a8f46a2897e38524
OnurKISACIK <onurkisacik1985@gmail.com>
2025-01-31 21:51:58 +0200

    NEW

--- 5TH DAY/1.9 testFolder.js
+++ 5TH DAY/1.9 testFolder.js
@@ -6,10 +6,53 @@ let arr1 = [1, 2, 3];
 arr1[50] = 10;
 console.log(arr1);
 */
+/*
 function a() {
   let arr = [1, 2, 3];
   arr.push(4);
   arr.push(7);
-  console.log(arr);
+
+  let arr1 = ["Onur", "Alex", "Ahmet"];
+  arr1.push("Alex");
+
+  let arr3 = [1, 2, 3];
+  arr3.push(4, 5, 6);
+  console.log(arr3);
 }
 a();
+
+*/
+
+/*
+let arr = [1, 2, 3, 4, 5];
+
+let a = arr.join("\n");
+console.log(a);
+*/
+/*
+function a() {
+  let total = 0;
+  let b = [1, 2, 3, 4, 5];
+  for (let i = 0; i < b.length; i++) {
+    let c = b[i];
+    total += c;
+  }
+  console.log(total);
+}
+a();
+*/
+/*
+function reverseAnArrayOfNumbers(n, arr) {
+  let arr1 = [];
+  let arr2 = [];
+  for (let i = 0; i < n; i++) {
+    arr1.push(arr[i]);
+  }
+
+  for (let j = arr1.length - 1; j >= 0; j--) {
+    arr2.push(arr[j]);
+  }
+  console.log(arr2.join(" "));
+}
+reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50, 60]);
+*/
